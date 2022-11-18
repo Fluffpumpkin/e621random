8a7c0cbe7f6d232267d42713fc71acf559f11e2b
Surfluff <61837182+Surfluff@users.noreply.github.com>
2022-11-18 10:06:27 -0700

    Add files via upload

---
+++ e621random.user.js
@@ -0,0 +1,54 @@
+// ==UserScript==
+// @name         e621 random post on every page
+// @namespace    n/a
+// @version      0.2
+// @description  adds random post button and hotkey to image pages, not just the search pages
+// @author       reBane
+// @match        https://e621.net/posts/*
+// @match        http://e621.net/posts/*
+// @match        https://e926.net/posts/*
+// @match        http://e926.net/posts/*
+// @run-at       document-end
+// @grant        none
+// ==/UserScript==
+
+var keypress = 82;
+var keypress2 = 190;
+var current = window.location.href;
+var tags = "";
+if(current.includes("?tags=")){
+    tags = current.split("?tags=")[1];
+}else{
+    tags = current.split("?q=")[1];
+}
+
+
+ window.onkeydown= function(gfg){
+     if(document.querySelector('input') != document.activeElement){
+        if(gfg.keyCode === keypress){
+            if(tags){
+                window.location.href = 'https://e621.net/posts/random?tags='+tags;
+            }else{
+                window.location.href = 'https://e621.net/posts/random';
+            }
+        };
+         if(gfg.keyCode === keypress2){
+            if(tags){
+                window.location.href = 'https://e621.net/posts/random?tags='+tags;
+            }else{
+                window.location.href = 'https://e621.net/posts/random';
+            }
+        };
+     };
+ }
+
+const getrandom = document.createElement("a");
+ if(tags){
+                getrandom.href = 'https://e621.net/posts/random?tags='+tags;
+            }else{
+                getrandom.href = 'https://e621.net/posts/random';
+            }
+const text = document.createTextNode("Random post");
+getrandom.appendChild(text);
+const element = document.getElementById("post-related-images");
+element.appendChild(getrandom);
