1327e5db2c4f482590295aef728cc3fd805d0c4c
Surfluff <61837182+Surfluff@users.noreply.github.com>
2023-09-06 14:52:18 -0700

    Update e621random.user.js
    Added support for the base page

--- e621random.user.js
+++ e621random.user.js
@@ -5,9 +5,9 @@
 // @description  adds random post button and hotkey to image pages, not just the search pages
 // @author       Surfluff
 // @match        https://e621.net/posts/*
-// @match        http://e621.net/posts/*
+// @match        https://e621.net/posts*
 // @match        https://e926.net/posts/*
-// @match        http://e926.net/posts/*
+// @match        https://e926.net/posts*
 // @run-at       document-end
 // @grant        none
 // ==/UserScript==
@@ -16,38 +16,64 @@ var keypress = 82;
 var keypress2 = 190;
 var current = window.location.href;
 var tags = "";
-if(current.includes("?tags=")){
-    tags = current.split("?tags=")[1];
+if(current.includes("tags=")){
+    tags = current.split("tags=")[1];
+}else if(current.includes("q=")){
+    tags = current.split("q=")[1];
 }else{
-    tags = current.split("?q=")[1];
+    tags=""
 }
 
 
  window.onkeydown= function(gfg){
      if(document.activeElement.tagName != "INPUT" && document.activeElement.tagName != "TEXTAREA"){
         if(gfg.keyCode === keypress){
-            if(tags){
-                window.location.href = 'https://e621.net/posts/random?tags='+tags;
-            }else{
-                window.location.href = 'https://e621.net/posts/random';
+            if(window.location.href.startsWith('https://e621.net')){
+                if(tags){
+                    window.location.href = 'https://e621.net/posts/random?tags='+tags;
+                }else{
+                    window.location.href = 'https://e621.net/posts/random';
+                }
+            }else if(window.location.href.startsWith('https://e926.net')){
+                if(tags){
+                    window.location.href = 'https://e926.net/posts/random?tags='+tags;
+                }else{
+                    window.location.href = 'https://e926.net/posts/random';
+                }
             }
         };
-         if(gfg.keyCode === keypress2){
-            if(tags){
-                window.location.href = 'https://e621.net/posts/random?tags='+tags;
-            }else{
-                window.location.href = 'https://e621.net/posts/random';
+        if(gfg.keyCode === keypress2){
+            if(window.location.href.startsWith('https://e621.net')){
+                if(tags){
+                    window.location.href = 'https://e621.net/posts/random?tags='+tags;
+                }else{
+                    window.location.href = 'https://e621.net/posts/random';
+                }
+            }else if(window.location.href.startsWith('https://e926.net')){
+                if(tags){
+                    window.location.href = 'https://e926.net/posts/random?tags='+tags;
+                }else{
+                    window.location.href = 'https://e926.net/posts/random';
+                }
             }
         };
      };
  }
 
 const getrandom = document.createElement("a");
- if(tags){
-                getrandom.href = 'https://e621.net/posts/random?tags='+tags;
-            }else{
-                getrandom.href = 'https://e621.net/posts/random';
-            }
+if(window.location.href.startsWith('https://e621.net')){
+    if(tags){
+        getrandom.href = 'https://e621.net/posts/random?tags='+tags;
+    }else{
+        getrandom.href = 'https://e621.net/posts/random';
+    }
+}else if(window.location.href.startsWith('https://e926.net')){
+    if(tags){
+        getrandom.href = 'https://e926.net/posts/random?tags='+tags;
+    }else{
+        getrandom.href = 'https://e926.net/posts/random';
+    }
+}
 const text = document.createTextNode("Random post");
 getrandom.appendChild(text);
 const element = document.getElementById("post-related-images");
